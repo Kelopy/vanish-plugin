b55bf4175a482313ce76a25e536366330752fce7
Kelopy <theytrank@gmail.com>
2022-06-22 17:15:10 +0100

    -#

--- src/main/java/me/kelopy/gone/commands/vanish.java
+++ src/main/java/me/kelopy/gone/commands/vanish.java
@@ -8,8 +8,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-
 public class vanish implements CommandExecutor {
 
     Gone plugin;
